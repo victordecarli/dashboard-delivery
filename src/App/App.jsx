afab85fdfde60928c236facde853716e92c83b26
Victor de Carli <victordecarliJAODELIVERY@MacBook-Air-de-Suporte.local>
2025-11-25 19:59:30 -0400

    feat: adiciona config de rotas

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,42 +1,102 @@
-import { useState } from 'react';
-import { Routes, Route, useLocation } from 'react-router';
-import { Cadastro } from '../pages/Cadastro/Cadastro';
-import { Login } from '../pages/Login/Login';
-import Pedidos from '../pages/Pedidos/Pedidos';
-import Localizacao from '../pages/localizacao/localizacao';
-import ContatoPesquisa from '../pages/Contato/ContatoPesquisa';
-import EsqueciSenha from "../pages/Login/esqueciSenha.jsx";
-import ResetPassword from "../pages/Login/reset-password.jsx";
-import Dashboard from '../pages/Dashboard/Dashboard';
+import { useEffect, useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router';
+import Restaurantes from '../pages/Restaurantes/Restaurantes.jsx';
+import PedidosLayout from '../pages/Pedidos/PedidosLayout';
+import AuthLayout from '../pages/auth/AuthLayout';
+import Pedidos from '../pages/Pedidos/Pedidos.jsx';
+import Login from '../pages/auth/Login/Login.jsx';
+import Cadastro from '../pages/auth/Cadastro/Cadastro.jsx';
+import Dashboard from '../pages/Dashboard/Dashboard.jsx';
+import Contato from '../pages/contato/Contato.jsx';
 import Layout from '../layout/layout';
 import '../global.css';
+import { supabase } from '../lib/supabase.js';
 
-export function App() {
-  const [userLogin, setUserLogin] = useState(false);
-  const location = useLocation();
-
-  const publicRoutes = ["/login", "/cadastro", "/esqueciSenha", "/reset-password"];
-
-  const isPublicRoute = publicRoutes.includes(location.pathname);
-
-  if (!userLogin && !isPublicRoute) {
-  return <Login setUserLogin={setUserLogin} />;
+// Componente para proteger rotas que requerem autenticação
+function ProtectedRoute({ children, session }) {
+  if (!session) {
+    return <Navigate to="/auth/login" replace />;
+  }
+  return children;
 }
 
+export function App() {
+  const [session, setSession] = useState(null);
+
+  const fetchSession = async () => {
+    const currentSession = await supabase.auth.getSession();
+    setSession(currentSession.data);
+  };
+
+  useEffect(() => {
+    fetchSession();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      },
+    );
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
+
+  // const logout = async () => {
+  //   await supabase.auth.signOut();
+  // };
 
   return (
-    <Layout userLogin={userLogin}>
+    <Layout userLogin={session}>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/pedidos" element={<Pedidos />} />
-        <Route path="/restaurantes" element={<Localizacao />} />
-        <Route path="/contato" element={<ContatoPesquisa />} />
+        <Route
+          index
+          element={
+            <ProtectedRoute session={session}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/contato"
+          element={
+            <ProtectedRoute session={session}>
+              <Contato />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/restaurantes"
+          element={
+            <ProtectedRoute session={session}>
+              <Restaurantes />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path="/login" element={<Login setUserLogin={setUserLogin} />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-       <Route path="/esqueciSenha" element={<EsqueciSenha />} />
-       <Route path="/reset-password" element={<ResetPassword />} />
+        <Route
+          path="pedidos"
+          element={
+            <ProtectedRoute session={session}>
+              <PedidosLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Pedidos />} />
+          {/* <Route path="novo" element={<OrderForm />} /> */}
+          {/* <Route path=":pid" element={<PedidoDetalhes />} /> */}
+          {/* <Route path=":pid/editar" element={<OrderForm />} /> */}
+        </Route>
 
+        <Route path="auth" element={<AuthLayout session={session} />}>
+          <Route path="login" element={<Login />} />
+          <Route path="cadastro" element={<Cadastro />} />
+        </Route>
+
+        <Route
+          path="*"
+          element={<h2 className="p-6">Página não encontrada</h2>}
+        />
       </Routes>
     </Layout>
   );
